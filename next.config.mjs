c71de6c42dfd8730e786ba18b2aef0c15000a6c7
Inseok Kim <js2121good@gmail.com>
2025-11-09 02:35:47 +0900

    vercel error

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   env: {
     NEXT_PUBLIC_API_SERVER: process.env.NEXT_PUBLIC_API_SERVER,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./src"),
-    };
-    return config;
   },
 };
 
